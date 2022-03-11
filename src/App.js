e85749a973696466208fb287dc95a91e2d5bf306
andreyscott <andreyscott>
2022-03-11 17:34:22 +0100

    made

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 //import {faInstagram } from "@fortawesome/free-solid-svg-icons";
-import {faTwitter} from "@fortawesome/free-brands-svg-icons";
+//import {faTwitter} from "@fortawesome/free-brands-svg-icons";
 
 
 export default function App() {
   
+  const [count, setCount] = useState('');
 
-  return <div className="icons">
- <a href="https://twitter/andreyscott5"
- className="facebook social">
-<FontAwesomeIcon icon={faTwitter} size="2x" />
-</a>
-{/* <a href="https://www.instagram.com/andrayscott/"
-className="instagram social">
- <FontAwesomeIcon icon={faInstagram} size="2x" />
-</a> */}
+  return(
+    <div>
+      <h1>Count: {count}</h1> <div onClick={() => setCount(currentCount => currentCount +1)}>+</div>
+      <button onClick={() => setCount(currentCount => currentCount -1)}>-</button>
+      <a href="https://app.daily.dev/AndreyScott"><img src="https://api.daily.dev/devcards/9d330d22d197425cb2abff118b0f3f13.png?r=kxb" width="400" alt="Andrew Irorere's Dev Card"/></a>
+      
+      </div>
 
-</div>;
+  )
+
+
 }
